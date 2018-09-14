cdcc41f540dce79e4ab1058a2155f63ff137fe63
Tim Woolford <tim.woolford@gmail.com>
2018-09-14 14:50:57 +0100

    Functions from sonique env

---
+++ sonique/alias.zsh
@@ -0,0 +1,6 @@
+alias mi='mvn clean install'
+alias mist='mvn clean install -DskipTests'
+alias mih2='mvn clean install -DuseH2=true'
+alias mio='mvn clean install -PuseOracle'
+alias mior='mvn clean install -PuseOracle,rebuildDatabase'
+alias mistor='mvn package -PuseOracle,rebuildDatabase -pl ${PWD##*/}-sql'
