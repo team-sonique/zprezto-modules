c7d2af7c480a7e32c766d1d178cac2c3b3430803
Tim Woolford <tim.woolford@gmail.com>
2018-10-20 12:30:57 +0100

    Storing applications in zstyle

--- shoehorn/init.zsh
+++ shoehorn/init.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
+typeset -gA APPLICATIONS
+zstyle -a ':prezto:module:shoehorn' applications 'APPLICATIONS'
+
 source "${0:h}/artifactory.zsh"
 source "${0:h}/docker.zsh"
 source "${0:h}/completion_helpers.zsh"
