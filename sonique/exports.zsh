c0f4430b4e0333c98f0b7002e55aee5cabd8fbe8
imke <imke.himstedt@sky.uk>
2019-06-21 10:43:39 +0100

    TECH [Pete, Paul] Typo fix

--- sonique/exports.zsh
+++ sonique/exports.zsh
@@ -2,7 +2,7 @@ export FIREFOX_HOME=/Applications/Firefox.app/Contents/MacOS
 export DATABASE_URL=jdbc:oracle:thin:@//localhost:1521/db1
 export VDC_DATABASE_URL=jdbc:oracle:thin:@//localhost:1525/db1
 
-export JAVA_TOOL_OPTIONS=:-Dapple.awt.UIElement=true
+export JAVA_TOOL_OPTIONS=-Dapple.awt.UIElement=true
 
 export SVN_EDITOR=vim
 export SHOEHORN_BUILD_DIR=/tmp/shoehorn
