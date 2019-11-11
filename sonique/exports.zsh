74b7a7d2860566400ea98370282c4043850d1679
Sonique <team.sonique.sns@bskyb.com>
2019-11-11 17:06:41 +0000

    Remove JAVA_TOOL_OPTIONS

--- sonique/exports.zsh
+++ sonique/exports.zsh
@@ -2,8 +2,6 @@ export FIREFOX_HOME=/Applications/Firefox.app/Contents/MacOS
 export DATABASE_URL=jdbc:oracle:thin:@//localhost:1521/db1
 export VDC_DATABASE_URL=jdbc:oracle:thin:@//localhost:1525/db1
 
-export JAVA_TOOL_OPTIONS=-Dapple.awt.UIElement=true
-
 export SVN_EDITOR=vim
 export SHOEHORN_BUILD_DIR=/tmp/shoehorn
 export SENCHA_CMD_DIR=~/bin/Sencha/Cmd
