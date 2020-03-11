51dbd375cea54dc09c581ce5bf03f76beedd0560
Sonique <team.sonique.sns@bskyb.com>
2020-03-11 10:03:18 +0000

    TECH [TimW] :: dont need firefox

--- sonique/exports.zsh
+++ sonique/exports.zsh
@@ -1,8 +1,7 @@
-export FIREFOX_HOME=/Applications/Firefox.app/Contents/MacOS
 export DATABASE_URL=jdbc:oracle:thin:@//localhost:1521/db1
 export VDC_DATABASE_URL=jdbc:oracle:thin:@//localhost:1525/db1
 
 export SVN_EDITOR=vim
 export SHOEHORN_BUILD_DIR=/tmp/shoehorn
 export SENCHA_CMD_DIR=~/bin/Sencha/Cmd
-export PATH=.:$FIREFOX_HOME:$PATH:$SENCHA_CMD_DIR
+export PATH=.:$PATH:$SENCHA_CMD_DIR
