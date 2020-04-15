2ced507f051ececbe52feac315d473aa18fd2665
Tim Woolford <tim.woolford@gmail.com>
2020-04-15 17:12:25 +0100

    TECH [TimW] :: added gpgEncrypt function, simplified some completions

--- sns-docker/init.zsh
+++ sns-docker/init.zsh
@@ -1,7 +1,12 @@
 DOCKER_CONFIG=~/Library/Group\ Containers/group.com.docker/settings.json
 
-if [[ -e "${DOCKER_CONFIG}" ]] ; then
+if [[ -e "${DOCKER_CONFIG}" ]]; then
+  BEFORE="$(md5 -q ${DOCKER_CONFIG})"
   CONFIG="$(jq -r '.filesharingDirectories = ( ( .filesharingDirectories + ["/data", "/logs"] ) | unique )' "${DOCKER_CONFIG}")"
-  echo "${CONFIG}" >! "${DOCKER_CONFIG}"
-fi
+  AFTER="$(echo ${CONFIG} | md5 -q)"
 
+  if [[ "${BEFORE}" != "${AFTER}" ]]; then
+    echo "${CONFIG}" >! "${DOCKER_CONFIG}"
+    echo "$FX[bold]$FG[red]DOCKER CONFIG FILE UPDATED!$FG[white]$FX[no-bold] - please restart docker!"
+  fi
+fi
--- sonique/exports.zsh
+++ sonique/exports.zsh
@@ -5,3 +5,5 @@ export SVN_EDITOR=vim
 export SHOEHORN_BUILD_DIR=/tmp/shoehorn
 export SENCHA_CMD_DIR=~/bin/Sencha/Cmd
 export PATH=.:$PATH:$SENCHA_CMD_DIR
+
+export ENCRYPTOR_IMAGE="repo.sns.sky.com:8186/dost/gpg-string-encryptor:22"
