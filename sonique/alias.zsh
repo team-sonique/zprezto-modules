d781196efc85ffd56a2bc41d93e70cfa52be2549
Sonique <team.sonique.sns@bskyb.com>
2020-06-26 10:06:30 +0100

    TECH [DanF, ChenL] :: Added alias to browse local activemq

--- sonique/alias.zsh
+++ sonique/alias.zsh
@@ -12,4 +12,5 @@ alias removeKnownHostBuildAgents='sed -i "" "/^ba*/d" ~/.ssh/known_hosts'
 alias stopoas='sudo /usr/local/McAfee/AntiMalware/VSControl stopoas'
 alias startoas='sudo /usr/local/McAfee/AntiMalware/VSControl startoas'
 alias resetVpn='sudo killall vpnagentd'
+alias amqbrowse='activemq browse --amqurl tcp://localhost:61616'
 
