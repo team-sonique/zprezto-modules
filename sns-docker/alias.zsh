9877bd571a1ee60152ff1e31636c13e19d06f5b5
Tim Woolford <tim.woolford@gmail.com>
2020-07-29 16:47:06 +0100

    fix docker aliases

--- sns-docker/alias.zsh
+++ sns-docker/alias.zsh
@@ -1,2 +1,2 @@
-gcrLogin='cat ~/.gcloud/GCR_SERVICE_ACCOUNT_KEY.json | docker login -u _json_key --password-stdin eu.gcr.io'
-gcrLogout='docker logout eu.gcr.io'
+alias gcrLogin='cat ~/.gcloud/GCR_SERVICE_ACCOUNT_KEY.json | docker login -u _json_key --password-stdin eu.gcr.io'
+alias gcrLogout='docker logout eu.gcr.io'
