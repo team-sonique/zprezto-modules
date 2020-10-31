ce7142a194cc2741a372e10d1a7cc7b53107c95c
Tim Woolford <tim.woolford@gmail.com>
2020-10-31 15:51:03 +0000

    Better error handling on jq

--- shoehorn/gocd.zsh
+++ shoehorn/gocd.zsh
@@ -3,7 +3,7 @@ function _get_latest_version_gocd {
     {
         function $0_getVersion {
             local stage=${1} fileName=${app}-${2}
-            echo $(jq -r "[.pipelines[] | select( .stages[] | .name == \"${stage}\" and .status == \"Passed\")][0].label" ${shoeTmp}/${fileName%-}.json)
+            echo $(jq -r "[.pipelines[] | select( .stages[] | .name == \"${stage}\" and .status == \"Passed\")][0].label" ${shoeTmp}/${fileName%-}.json 2> /dev/null)
         }
 
         if [[ ( ! -e ${shoeTmp}/${app}.json ) || -z $(find ${shoeTmp}/${app}.json -mmin -${_gocd_cache_minutes}) ]]; then
